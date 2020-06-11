f9e22fb3c3b5df774831df65631a749f410fc8b7
Stine K. Petersen <spirrandersen@gmail.com>
2020-06-11 13:38:40 +0200

    clean up

--- server/routes/users.js
+++ server/routes/users.js
@@ -119,67 +119,6 @@ router.post("/signup" ,[
 
 // Login
 router.post("/login",  (req, res) => {
-    // const {email, password} = req.body
-
-    try {
-        // Mongo query 
-        let email = 'a@a.com'
-        usersCollection.findOne({email: "a@a.com"}).toArray((err, ajUser) => {
-            if (err) {
-                res.status(500).send([]) // edited
-                return
-            }
-            // Returning a response in ajUsers with all users in the database
-            return res.send(ajUser)
-        })
-
-    } catch (error) {
-        // Should it be here? TODO: Test this
-        process.on("uncaughtException", (err, data) => {
-            if (err) {
-                console.log("critical error, yet system keeps running");
-                return;
-            }
-        });
-    }
-
-
-
-
-    
-
-    // const find = async () => {
-    //     try {
-    //         const user = await usersCollection.findOne({ email: email, password: password })
-    //     } catch (err) {
-    //         console.log(err)
-    //     }
-    // }
-
-    // usersCollection.findOne({ email, password }), function(err, unknown) {
-    //     return res.send('found email', email)
-    // }
-
-    // const form = formidable({multiples: true});
-
-    // form.parse(req, (err, fields) => {
-    //     if(err){console.log('no user'); return}
-    //     let email = fields.txtEmail
-    //     let password = fields.txtPassword
-        // if ( !email || !password || email !== password ) { // validating wrong
-        //     console.log('x')
-        //     return res.status(400).end()
-        // }
-                
-        // const token = jwt.sign({email}, jwtKey, {
-        //         algorithm: "HS256",
-        //         expiresIn: jwtExpirySecounds
-        // })
-        //     console.log("Token:", token)
-    
-        // return res.status(200).send(`${email} ${password} `) // ${token}
-
-    // })
 
 });
 
